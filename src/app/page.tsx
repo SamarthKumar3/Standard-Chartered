c5896920256a20e7cfce74626c6907f3b29ca47f
Jayatri Banerjee <jbanerjeedgp01022003@gmail.com>
2024-03-14 17:33:23 +0530

    fix: bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,11 @@
-// "use client";
-// import Link from "next/link";
-// import { TypewriterEffectSmooth } from "../ui/typewriter-effect";
-
-
-
-// export default function Page() {
-//   const words = [
-//     {
-//       text: "Build",
-//     },
-//     {
-//       text: "your",
-//     },
-//     {
-//       text: "Trust",
-//     },
-//     {
-//       text: "with",
-//     },
-//     {
-//       text: "VerifyFin.",
-//       className: "text-blue-500 dark:text-blue-500",
-//     },
-//   ];
-//   return (
-//     <div className="flex flex-col items-center justify-center h-[40rem]  ">
-//       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-//         The road to seamless experience starts from here
-//       </p>
-//       <TypewriterEffectSmooth words={words} />
-//       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-//         <Link
-//           className="w-40 h-10 px-9 py-2 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm"
-//           href="/chatbot"
-//         >
-//           Get started
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
 "use client";
-import { useState, useEffect } from 'react';
-import { TypewriterEffectSmooth } from '../ui/typewriter-effect';
+
+import { useState, useEffect } from "react";
+import { TypewriterEffectSmooth } from "../ui/typewriter-effect";
+import next from "next";
 
 export default function Page() {
   const words = [
-<<<<<<< HEAD
-    { text: 'Build' },
-    { text: 'your' },
-    { text: 'Trust' },
-    { text: 'with' },
-    { text: 'VerifyFin.', className: 'text-blue-500 dark:text-blue-500' },
-=======
     {
       text: "Build",
     },
@@ -69,7 +22,6 @@ export default function Page() {
       text: "VerifyFin.",
       className: "text-blue-500 dark:text-blue-500 overflow-hidden",
     },
->>>>>>> ee23ba4beb9cac68881a112b619052d45bf6506a
   ];
 
   const [showChatbot, setShowChatbot] = useState(false);
@@ -80,52 +32,41 @@ export default function Page() {
 
   useEffect(() => {
     if (showChatbot) {
-      document.getElementById('hero')?.classList.add('dark');
+      document.getElementById("hero")?.classList.add("dark");
     } else {
-      document.getElementById('hero')?.classList.remove('dark');
+      document.getElementById("hero")?.classList.remove("dark");
     }
 
     return () => {
-      document.getElementById('hero')?.classList.remove('dark');
+      document.getElementById("hero")?.classList.remove("dark");
     };
   }, [showChatbot]);
 
   return (
-<<<<<<< HEAD
     <>
-      <div className="flex flex-col items-center justify-center h-[40rem]" id='hero' >
-        <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
+      <div
+        className="flex flex-col items-center justify-center h-[40rem] overflow-hidden"
+        id="hero"
+      >
+        <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base overflow-hidden ">
           The road to seamless experience starts from here
         </p>
         <TypewriterEffectSmooth words={words} />
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
           <button
-            onClick={handleGetStartedClick}
             className="w-40 h-10 px-9 py-2 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm"
-            style={{ display: showChatbot ? 'none' : 'block' }}
+            onClick={handleGetStartedClick}
           >
             Get started
           </button>
         </div>
-
-=======
-    <div className="flex flex-col items-center justify-center h-[40rem] overflow-hidden ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base overflow-hidden ">
-        The road to seamless experience starts from here
-      </p>
-      <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <Link
-          className="w-40 h-10 px-9 py-2 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm"
-          href="/chatbot"
-        >
-          Get started
-        </Link>
->>>>>>> ee23ba4beb9cac68881a112b619052d45bf6506a
       </div>
       {showChatbot && (
         <div className="fixed bottom-4 right-4 w-1/3">
-          <div id="tars-fullpage-container" className="tars-fullpage-container-class rounded-lg ">
+          <div
+            id="tars-fullpage-container"
+            className="tars-fullpage-container-class rounded-lg "
+          >
             <iframe
               id="tars-fullpage"
               width="100%"
@@ -137,6 +78,6 @@ export default function Page() {
           <script src="https://tars-file-upload.s3.amazonaws.com/bulb/js/fullpage.js"></script>
         </div>
       )}
-    </> 
+    </>
   );
 }
