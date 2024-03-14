56c2d57e41523eb6ee3e5ce63859668f1727d337
Jayatri Banerjee <jbanerjeedgp01022003@gmail.com>
2024-03-14 15:56:11 +0530

    fix: confirm page and navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NavbarDemo } from "@/components/navbar";
+import { Navbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Navbar />
+
       <body className={inter.className}>{children}</body>
     </html>
   );
